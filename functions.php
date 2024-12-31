f35d2f2a098226abff5b5a0d9e534b0115450adf
AlexxBelous <connectionwithme1@gmail.com>
2024-12-31 11:09:26 +0200

    start project

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+
+function print_arr($data)
+{
+    echo '<pre>';
+    print_r($data);
+    echo '</pre>';
+}
--- index.php
+++ index.php
@@ -6,6 +6,4 @@ require __DIR__ . '/vendor/autoload.php';
 
 
 
-
-
 require_once 'views/index.tpl.php';
